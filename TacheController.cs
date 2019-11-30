145135ff884de0715b13bd40792b16f47f7f7fe3
inesISSAOUI <47125388+inesISSAOUI@users.noreply.github.com>
2019-11-30 21:31:59 +0100

    Add files via upload

---
+++ TacheController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class TacheController : Controller
+    {
+        // GET: Tache
+        public ActionResult Index()
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:9080");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("PI_Timesheet-web/ws/tache").Result;
+            if (response.IsSuccessStatusCode) { ViewBag.result = response.Content.ReadAsAsync<IEnumerable<Tache>>().Result; }
+            else { ViewBag.result = "error"; }
+            return View();
+        }
+
+        // GET: Tache/Details/5
+        public ActionResult Details(int id)
+
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:9080");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("ws/tache/{id}").Result;
+            if (response.IsSuccessStatusCode) { ViewBag.result = response.Content.ReadAsAsync<Tache>().Result; }
+            else { ViewBag.result = "error"; }
+            return View();
+        }
+
+        // GET: Tache/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tache/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tache/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Tache/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tache/Delete/5
+        public ActionResult Delete(int id)
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:9080/PI_Timesheet-web");
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.DeleteAsync("ws/tache/{id}").Result;
+            if (response.IsSuccessStatusCode) { ViewBag.result = "Tache deleted !"; }
+            else { ViewBag.result = "error !"; }
+            return View();
+        }
+
+        // POST: Tache/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
