aa12113380cc5ff934e7a3bec2ff45c5f1197730
seifeddine ben fradj <47224030+abdelkefianas@users.noreply.github.com>
2019-12-03 19:56:17 +0100

    formation

---
+++ Pidev.Web/Controllers/formationController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+using Pidev.Data;
+
+namespace Pidev.Web.Controllers
+{
+    public class formationController : Controller
+    {
+        // GET: formation
+        public ActionResult affpart()
+        {
+            HttpClient Client = new HttpClient();
+
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("http://localhost:9080/pidev-web/api/formations").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.result = response.Content.ReadAsAsync<IEnumerable<formation>>().Result;
+
+            }
+            else
+            {
+                ViewBag.result = "error";
+
+            }
+
+
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.DeleteAsync("http://localhost:9080/pidev-web/api/formations/" + id.ToString()).Result;
+
+            return RedirectToAction("affpart");
+        }
+        // POST: Project/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+
+            return View();
+
+        }
+    }
+}
---
+++ Pidev.Web/Models/formation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pidev.Web.Models
+{
+    public class formation
+    {
+        public int id { get; set; }
+
+       
+        public string description { get; set; }
+
+    
+        public string duration { get; set; }
+
+        public int nbPlaceDispo { get; set; }
+
+     
+        public string nomFormation { get; set; }
+    }
+}
